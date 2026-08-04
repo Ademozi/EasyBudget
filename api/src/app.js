4ea0bd00aa376e177aa4c93b0b14230231697de8
Ademozi <ademoviadem@gmail.com>
2026-08-04 20:53:56 +0100

    Implement Register User

--- api/src/app.js
+++ api/src/app.js
@@ -2,10 +2,14 @@ const express = require("express");
 
 const app = express();
 
+const authRoutes = require("./routes/authRoutes");
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("EasyBudget API is running...");
 });
+
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
---
+++ api/src/controllers/authController.js
@@ -0,0 +1,57 @@
+const bcrypt = require("bcrypt");
+//const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+//const generateToken = require("../utils/generateToken");
+
+const register = async (req, res) => {
+    try {
+
+        const { username, email, password } = req.body;
+
+        if (!username || !email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required."
+            });
+        }
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({
+                success: false,
+                message: "User with this email already exists."
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+        });
+
+        await newUser.save();
+        res.status(201).json({
+            message: "User registered successfully",
+            user: {
+                id: newUser._id,
+                username: newUser.username,
+                email: newUser.email
+            }
+        });
+
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({ 
+            success: false,
+            message: "Server error" 
+        });
+    }
+};
+
+module.exports = {
+    register,
+};
