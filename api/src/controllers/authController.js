444b7d9c3a904299a07c5a30a89e26cd1d2327a8
Ademozi <ademoviadem@gmail.com>
2026-08-05 10:11:43 +0100

    Create the generate token function

--- api/src/controllers/authController.js
+++ api/src/controllers/authController.js
@@ -52,6 +52,45 @@ const register = async (req, res) => {
     }
 };
 
+const login = async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Email and password are required."
+            });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid email or password."
+            });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid email or password."
+            });
+        } 
+
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({ 
+            success: false,
+            message: "Server error" 
+        });
+    }
+};
+
 module.exports = {
     register,
 };
