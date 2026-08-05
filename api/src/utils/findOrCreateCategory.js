e84c4b15e123324b4ffeb6baf04c345a2269bb9f
Ademozi <ademoviadem@gmail.com>
2026-08-05 19:21:49 +0100

    Implemenrt findOrCreateCategory

--- api/src/utils/findOrCreateCategory.js
+++ api/src/utils/findOrCreateCategory.js
@@ -1,4 +1,22 @@
-const category = await findOrCreateCategory(
-    req.user._id,
-    categoryName
-);
+const Category = require("../models/Category");
+
+const findOrCreateCategory = async (userId, categoryName) => {
+
+    const normalizedName = categoryName.trim().toLowerCase();
+
+    let category = await Category.findOne({
+        user: userId,
+        name: normalizedName
+    });
+
+    if (!category) {
+        category = await Category.create({
+            user: userId,
+            name: normalizedName
+        });
+    }
+
+    return category;
+};
+
+module.exports = findOrCreateCategory;
