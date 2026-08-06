fc6f001b155ce4e85e1290161de3dc0ad0c4a8bb
Ademozi <ademoviadem@gmail.com>
2026-08-06 21:49:32 +0100

    Optimization

--- api/src/controllers/transactionController.js
+++ api/src/controllers/transactionController.js
@@ -59,9 +59,7 @@ const getTransactions = async (req, res) => {
         // Calculate the number of documents to skip for pagination
         const skip = (Number(page) - 1) * Number(limit);
 
-        // Get total count of transactions for the user (and type if provided)
-        const totalTransactions = await Transaction.countDocuments(filter);
-
+        
         // Calculate total pages based on the total count and limit
         // math.ceil() is used to round up to the nearest whole number, 
         // ensuring that any remaining transactions that don't fill a complete page are still accounted for in the total page count.
@@ -71,12 +69,19 @@ const getTransactions = async (req, res) => {
         const hasPreviousPage = Number(page) > 1;
 
 
-        // Get transactions
-        const transactions = await Transaction.find(filter) // Filter transactions
-            .populate("category") // istead of showing only the id of category, populate will show all the category details
-            .sort({ date: -1 }) // Sort transactions by date (Newest first)
-            .skip(skip)
-            .limit(Number(limit));
+        // Optimization: Instead of making two separate database calls (one for counting and one for fetching),
+        // Since they don't depend on each other, we can run them at the same time using Promise.all(), 
+        // which reduces the total response time.
+        const [totalTransactions, transactions] = await Promise.all([
+            // Get total count of transactions for the user (and type if provided)
+            Transaction.countDocuments(filter),
+            // Get transactions for the user (and type if provided) with pagination
+            Transaction.find(filter) // Filter transactions
+                .populate("category") // istead of showing only the id of category, populate will show all the category details
+                .sort({ date: -1 }) // Sort transactions by date (Newest first)
+                .skip(skip)
+                .limit(Number(limit))
+        ]);
 
         res.status(200).json({
             success: true,
