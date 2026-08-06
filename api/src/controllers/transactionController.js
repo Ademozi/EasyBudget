b5d5fc423bf8b62ea967f52e38185666cfaed770
Ademozi <ademoviadem@gmail.com>
2026-08-06 10:36:52 +0100

    Implement getTransactions

--- api/src/controllers/transactionController.js
+++ api/src/controllers/transactionController.js
@@ -36,6 +36,34 @@ const createTransaction = async (req, res) => {
     }
 };
 
+const getTransactions = async (req, res) => {
+
+    try {
+
+        const transactions = await Transaction
+            .find({ user: req.user._id }) // Filter transactions by the authenticated user's ID
+            .populate("category") // istead of showing only the id of category, populate will show all the category details
+            .sort({ date: -1 }); // Sort transactions by date (Newest first)
+
+        res.status(200).json({
+            success: true,
+            count: transactions.length,
+            transactions
+        });
+
+    }
+    catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            success: false,
+            message: "Server error"
+        });
+    }
+};
+
 module.exports = {
-    createTransaction
+    createTransaction,
+    getTransactions
 };
--- api/src/routes/transactionRoutes.js
+++ api/src/routes/transactionRoutes.js
@@ -3,8 +3,13 @@ const router = express.Router();
 
 const protect = require("../middleware/authMiddleware");
 
-const { createTransaction } = require("../controllers/transactionController");
+const { createTransaction, getTransactions } = require("../controllers/transactionController");
 
-router.post("/", protect, createTransaction);
+// router.post("/", protect, createTransaction);
+// router.get("/", protect, getTransactions);
+router
+    .route("/")
+    .post(protect, createTransaction)
+    .get(protect, getTransactions);
 
 module.exports = router;
