f210d78f0393d24bda477f10afb75e9413364a5f
Ademozi <ademoviadem@gmail.com>
2026-08-09 10:44:11 +0100

    add monry to goal route

--- api/src/routes/goalRoutes.js
+++ api/src/routes/goalRoutes.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const protect = require("../middleware/authMiddleware");
 const validate = require("../middleware/validate");
 
-const { createGoalValidation } = require("../validators/goalValidator");
-const { createGoal, getGoals } = require("../controllers/goalController");
+const { createGoalValidation, addMoneyValidation } = require("../validators/goalValidator");
+const { createGoal, getGoals, addMoneyToGoal } = require("../controllers/goalController");
 
 router.post("/", protect, createGoalValidation, validate, createGoal);
 router.get("/", protect, getGoals);
+router.patch("/:id/ad-money", protect, addMoneyValidation, validate, addMoneyToGoal);
 
 module.exports = router;
