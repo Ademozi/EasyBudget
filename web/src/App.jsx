303985a3f75119465e9ba3810fcb907a0ef328c6
Ademozi <ademoviadem@gmail.com>
2026-08-10 10:22:41 +0100

    Start the frontend project and implement  App.jsx

---
+++ web/src/App.jsx
@@ -0,0 +1,48 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function Dashboard() {
+    return (
+        <h1>Dashboard</h1>
+    );
+}
+
+function Login() {
+    return (
+        <h1>Login</h1>
+    );
+}
+
+function Register() {
+    return (
+        <h1>Register</h1>
+    );
+}
+
+function Transactions() {
+    return (
+        <h1>Transactions</h1>
+    );
+}
+
+function Goals() {
+    return (
+        <h1>Goals</h1>
+    );
+}
+
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/transactions" element={<Transactions />} />
+                <Route path="/goals" element={<Goals />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
